372738e78ac27bc24ee0ed6f36be926abf94c66a
Hiroaki Nakamura <hnakamur@gmail.com>
2020-09-03 19:02:36 +0900

    List all pids in procfs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+#[macro_use]
+extern crate lazy_static;
+
+use async_fs::read_dir;
+use futures_lite::stream::StreamExt;
+use regex::Regex;
+use std::io;
+
 fn main() {
     std::env::set_var("SMOL_THREADS", format!("{}", num_cpus::get()));
 
-    let task = smol::spawn(async { 1 + 2 });
-
     smol::block_on(async {
-        println!("{}", task.await);
+        psgrep().await.unwrap();
     });
+}
+
+async fn psgrep() -> io::Result<()> {
+    lazy_static! {
+        static ref PID_RE: Regex = Regex::new(r"^\d+$").unwrap();
+    }
+
+    let mut entries = read_dir("/proc").await?;
+    while let Some(res) = entries.next().await {
+        let entry = res?;
+        if PID_RE.is_match(entry.file_name().to_str().unwrap()) {
+            println!("path={:?}", entry.path());
+        }
+    }
+    Ok(())
 }
