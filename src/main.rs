dd637ed88446a046a9fd9441aaadbbfc38596c09
Hiroaki Nakamura <hnakamur@gmail.com>
2020-09-03 22:25:19 +0900

    Add is_pid_entry predicate function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #[macro_use]
 extern crate lazy_static;
 
-use async_fs::read_dir;
+use async_fs::{read_dir, DirEntry};
 use futures_lite::stream::StreamExt;
 use regex::Regex;
-use std::io;
 
 fn main() {
     std::env::set_var("SMOL_THREADS", format!("{}", num_cpus::get()));
@@ -14,17 +13,20 @@ fn main() {
     });
 }
 
-async fn psgrep() -> io::Result<()> {
-    lazy_static! {
-        static ref PID_RE: Regex = Regex::new(r"^\d+$").unwrap();
-    }
-
+async fn psgrep() -> std::io::Result<()> {
     let mut entries = read_dir("/proc").await?;
     while let Some(res) = entries.next().await {
         let entry = res?;
-        if PID_RE.is_match(entry.file_name().to_str().unwrap()) {
+        if is_pid_entry(&entry) {
             println!("path={:?}", entry.path());
         }
     }
     Ok(())
+}
+
+fn is_pid_entry(entry: &DirEntry) -> bool {
+    lazy_static! {
+        static ref PID_RE: Regex = Regex::new(r"^\d+$").unwrap();
+    }
+    PID_RE.is_match(entry.file_name().to_str().unwrap())
 }
